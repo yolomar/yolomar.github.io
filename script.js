26eb8a08a6504f7877420e7d631288110284690d
yolomar <omarmukhtark1@hotmail.com>
2023-05-23 15:46:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $('#stop').click(function(){
       console.log(t);
       var rows = t.getElementsByTagName("tr");
       var r = rows[rows.length - 1];
-      console.log(r);
+      console.log(hours + ":" + minutes + ":" + seconds);
       r.parentNode.insertAfter(getTemplateRow(), r);
       clearTimeout(timex);
 });
